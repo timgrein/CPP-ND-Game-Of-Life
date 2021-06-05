aa03f69ce198c8c8982566741094c77df5fb6717
Tim Grein <tim@4greins.de>
2021-06-05 09:37:23 +0200

    Add the abstract class Grid2DRenderer.h to provide a general interface for rendering a Grid2D.

---
+++ include/Grid2DRenderer.h
@@ -0,0 +1,12 @@
+#ifndef GAMEOFLIFE_GRIDRENDERER_H
+#define GAMEOFLIFE_GRIDRENDERER_H
+
+#include "Grid2D.h"
+
+template <typename T>
+class Grid2DRenderer{
+public:
+    virtual void Render(Grid2D<T>* grid) const = 0;
+};
+
+#endif //GAMEOFLIFE_GRIDRENDERER_H
