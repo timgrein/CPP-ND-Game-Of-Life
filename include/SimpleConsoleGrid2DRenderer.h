bb5112e7496875b60e16efae67e8c17e3e6cda1f
Tim Grein <tim@4greins.de>
2021-06-05 09:38:33 +0200

    Add SimpleConsoleGrid2DRenderer, which is a convenient implementation of a Grid2DRenderer printing a grid to cout.

---
+++ include/SimpleConsoleGrid2DRenderer.h
@@ -0,0 +1,12 @@
+#ifndef GAMEOFLIFE_SIMPLECONSOLEGRID2DRENDERER_H
+#define GAMEOFLIFE_SIMPLECONSOLEGRID2DRENDERER_H
+
+#include "Grid2DRenderer.h"
+
+template <typename T>
+class SimpleConsoleGrid2DRenderer : public Grid2DRenderer<T>{
+public:
+    void Render(Grid2D<T>* grid) const;
+};
+
+#endif //GAMEOFLIFE_SIMPLECONSOLEGRID2DRENDERER_H
---
+++ src/SimpleConsoleGrid2DRenderer.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <iostream>
+#include <Cell.h>
+
+#include "SimpleConsoleGrid2DRenderer.h"
+
+template<typename T>
+void SimpleConsoleGrid2DRenderer<T>::Render(Grid2D<T>* grid) const {
+    for(int row = 0; row < grid->GetNumberOfRows(); row++){
+        for(int col = 0; col < grid->GetNumberOfCols(); col++){
+            std::cout << (*grid)(row, col);
+            bool is_not_last_col = col != (grid->GetNumberOfCols() - 1);
+            if(is_not_last_col){
+                std::cout << " ";
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+template class SimpleConsoleGrid2DRenderer<int>;
+template class SimpleConsoleGrid2DRenderer<Cell>;
