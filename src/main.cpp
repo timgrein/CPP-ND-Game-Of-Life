68f0dbfddac83ff92e018f2bff875315ba89b72d
Tim Grein <tim@4greins.de>
2021-06-06 15:07:39 +0200

    Add an example lambda implementation of a custom cell state supplier.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #include <algo/GameOfLife.h>
 
 int main() {
@@ -8,21 +9,25 @@ int main() {
     // default example
     GameOfLife::Simulate(grid_size, number_of_generations);
 
-    // custom hooks example
+    // custom lambda hooks example
     GameOfLife::Simulate(grid_size, number_of_generations,
                          []() -> void {
                              std::cout << "Custom start hook: started simulation" << std::endl;
                          },
                          [](int current_generation) -> void {
-                             std::cout << "Custom Loop hook: current generation #" << (current_generation + 1) << std::endl;
+                             std::cout << "Custom Loop hook: current generation #" << (current_generation + 1)
+                                       << std::endl;
                          },
                          []() -> void {
                              std::cout << "Custom end hook: ended simulation" << std::endl;
                          });
 
-
-
-
+    // custom lambda cell state supplier example
+    GameOfLife::Simulate(grid_size, number_of_generations,
+                         []() -> void { DefaultSimulationHooks().StartHook(); },
+                         [](int current_generation) -> void { DefaultSimulationHooks().LoopHook(current_generation); },
+                         []() -> void { DefaultSimulationHooks().EndHook(); },
+                         []() -> Cell { return Cell(true); });
 
     return 0;
 }
